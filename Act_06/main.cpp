395cb7d347eea2a4bd0296262a48a4e9f048dd21
vidalmichela16-commits <vidalmichela16@gmail.com>
2026-03-12 11:50:56 +0800

    main.cpp

--- Act_06/main.cpp
+++ Act_06/main.cpp
@@ -1,1 +1,22 @@
 
+  #include <Arduino.h>
+  const int ledPin = 13;
+
+  void setup() {
+      pinMode(ledPin, OUTPUT);
+      Serial.begin(9600);
+  }
+
+  void loop() {
+      if (Serial.available() > 0) {
+          char command = Serial.read();
+
+          if (command == '1') {
+              digitalWrite(ledPin, HIGH);
+          } else if (command == '0') {
+              digitalWrite(ledPin, LOW);
+          }
+      }
+  }
+
+  
